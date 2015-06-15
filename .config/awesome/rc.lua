22fbebb9fb1c7f91bf3253af668f5cfcd8eda356
Michael Aldridge <aldridge.mac@gmail.com>
2015-06-14 22:39:32 -0500

    Add nm-applet to awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -462,3 +462,15 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
+-- Convenience function for singleton executables
+function run_once(cmd)
+   findme = cmd
+   firstspace = cmd:find(" ")
+   if firstspace then
+      findme = cmd:sub(0, firstspace-1)
+   end
+   awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > /dev/null || (" .. cmd .. ")")
+end
+
+-- Start the nm-applet
+run_once("nm-applet")
