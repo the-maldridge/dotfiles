c503df4142a31167a0d037a436fc77bf97fe721e
Michael Aldridge <aldridge.mac@gmail.com>
2017-04-11 22:32:55 -0500

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -34,17 +34,3 @@
              (string= (substring path -3) ".el"))
         (load (file-name-sans-extension fullpath)))))))
 (load-directory "~/.emacs.d/pkg-conf")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (fill-column-indicator python-mode column-marker flycheck use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- .emacs.d/pkg-conf/whitespace.el
+++ .emacs.d/pkg-conf/whitespace.el
@@ -5,21 +5,3 @@
 ;; Other
 (setq require-final-newline t)
 (setq show-trailing-whitespace t)
-
-;; Fill column and indicator
-(use-package fill-column-indicator
-  :ensure t
-  :config
-  (setq fci-rule-column 80)
-  (setq fci-rule-color "darkblue")
-  )
-
-(define-globalized-minor-mode global-fci-mode fci-mode
-  (lambda ()
-    (if (and
-         (not (string-match "^\*.*\*$" (buffer-name)))
-         (not (eq major-mode 'dired-mode)))
-        (fci-mode 1))))
-(global-fci-mode 1)
-
-(add-hook 'after-change-major-mode-hook 'global-fci-mode)
